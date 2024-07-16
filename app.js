32281eba1b7bfe2c4a1c1812c0282004eac45e0b
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-07-16 08:13:27 -0700

    styled views

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000 || process.env.PORT;
 const fs = require("fs");
 const bodyParser = require("body-parser"); //POST, PATCH
 
@@ -9,28 +9,45 @@ const videogames_Data = require("./data/video-games.json");
 const user1games_Data = require("./data/user1-games");
 const user2games_Data = require("./data/user2-games");
 
-const userRoute = require("./routes/userRoutes");
+const accountRoute = require("./routes/accountRoutes");
+
 const users = require("./data/users");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ extended: true }));
+app.use("/public", express.static(__dirname + "/public"));
 
 app.set("views", "views");
 app.set("view engine", "ejs");
 
+//HOME
 app.get("/", (req, res) => {
   res.render("home", { data: videogames_Data });
 });
 app.get("/login", (req, res) => {
+  const username = req.body.username;
+  //   console.log(username);
   res.render("login");
 });
+
+//USER
 app.get("/user", (req, res) => {
   const username = req.body.username;
-  res.render("games", {
-    data: videogames_Data,
-    games: user1games_Data,
-    name: username,
-  });
+  //   console.log(username);
+  if (username == users.username)
+    res.render("games", {
+      data: videogames_Data,
+      games: user1games_Data,
+      name: username,
+    });
+  else {
+    res.render("games", {
+      data: videogames_Data,
+      games: user2games_Data,
+      name: username,
+    });
+  }
 });
 app.post("/user", (req, res) => {
   const username = req.body.username;
@@ -44,10 +61,11 @@ app.post("/user", (req, res) => {
   });
 });
 
+//ACCOUNT
 // app.get("/account", (req, res) => {
 //   res.render("account");
 // });
-app.use("/account", userRoute);
+app.use("/account", accountRoute);
 
 app.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
